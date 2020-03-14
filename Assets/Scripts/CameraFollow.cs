943a6f83dd758f85b47049fb4156adc27920cf6d
durgauri <minochndk@gmail.com>
2020-03-14 16:57:01 +0700

    first commit

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] float followSpeed;
+    Vector3 offset;
+    void Start()
+    {
+        offset = target.position - transform.position;
+    }
+    void FixedUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position, target.position - offset, Time.deltaTime * followSpeed);
+    }
+}
---
+++ Assets/Scripts/WinTrigger.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+public class WinTrigger : MonoBehaviour
+{
+    [SerializeField] string nextLevelName;
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(nextLevelName);
+        }
+    }
+}
